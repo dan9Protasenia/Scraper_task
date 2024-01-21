72b69a8a77ae8bbcde1f4a074e8a0c5162b23393
Daniil Protasenia <danikprotaseney@gmail.com>
2024-01-21 03:17:39 +0300

    feat(scraper): make hacker_news_scraper

--- src/selenium/hacker_news_scraper.py
+++ src/selenium/hacker_news_scraper.py
@@ -1,18 +1,32 @@
-from src.core.scraper_interface import WebScraperInterface
+import json
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from ..core.scraper_interface import WebScraperInterface
 
 
 class HackerNewsScraper(WebScraperInterface):
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
     def initialize_scraper(self):
-        pass
+        self.driver = webdriver.Chrome()
 
     def navigate_to_page(self, url: str):
-        pass
+        self.driver.get(url)
 
     def extract_data(self):
-        pass
+        posts = self.driver.find_elements(By.CSS_SELECTOR, "tr.athing .title a")
+        data = []
+        for post in posts:
+            link = post.get_attribute("href")
+            if not link.startswith("https://news.ycombinator.com/from?site="):
+                data.append({"title": post.text, "link": link})
+        return data
 
     def write_data(self, data, filename: str):
-        pass
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
     def apply_filters(self, **kwargs):
-        pass
+        ...
