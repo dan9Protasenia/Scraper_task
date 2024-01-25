1ba1d3080531250c779ecc9854c735abe25bcfeb
Daniil Protasenia <danikprotaseney@gmail.com>
2024-01-25 21:30:48 +0300

    feat(hack_async): add hacker_news_scraper.py

--- src/selenium/asynchronous/hacker_news_scraper.py
+++ src/selenium/asynchronous/hacker_news_scraper.py
@@ -1,18 +1,23 @@
-from Scraper_task.src.core.schemas.data import Article
-from Scraper_task.src.scraper_interface import WebScraperInterface
 from selenium.webdriver.common.by import By
 
+from ...core.schemas.data import Article
+from ...scraper_interface_async import WebScraperInterfaceAsync
 
-class HackerNewsScraper(WebScraperInterface):
-    def extract_data(self):
+
+class HackerNewsScraperAsync(WebScraperInterfaceAsync):
+    async def extract_data(self, html: str):
         posts_elements = self.driver.find_elements(By.CSS_SELECTOR, "tr.athing .title a")
         data = []
+
         for post in posts_elements:
             link = post.get_attribute("href")
-            if not link.startswith("https://news.ycombinator.com/from?site="):
-                article = Article(title=post.text, link=link)
+            title = post.text
+
+            if title and link in link and not link.startswith("https://news.ycombinator.com/from?site="):
+                article = Article(title=title, link=link)
                 data.append(article)
+
         return data
 
-    def apply_filters(self, **kwargs):
+    async def apply_filters(self, **kwargs):
         ...
