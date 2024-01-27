995cd77f0beb336025d0e393b02b146099bf3ed0
Daniil Protasenia <danikprotaseney@gmail.com>
2024-01-27 03:37:13 +0300

    refactor(main): rem print and loggs also edit structure

--- src/main.py
+++ src/main.py
@@ -1,67 +1,67 @@
+import logging
 import sys
 
-from .b4s.hh_scraper import HhScraper
-from .b4s.nyt_scraper import NYTimesScraperBS
-from .selenium.hacker_news_scraper import HackerNewsScraper
-from .selenium.rabota_scraper import RabotaScraper
+from src.b4s.sync.hh_scraper import HhScraper
+from src.b4s.sync.nyt_scraper import NYTimesScraperBS
+from src.logger_config import setup_logging
+from src.selenium.sync.hacker_news_scraper import HackerNewsScraper
+from src.selenium.sync.rabota_scraper import RabotaScraper
+
+logger = logging.getLogger(__name__)
+
+
+def run_nyt_scraper():
+    url = "https://www.nytimes.com"
+    filename = "nyt_data.json"
+    scraper = NYTimesScraperBS()
+    scraper.run(url, filename)
+
+
+def run_hh_scraper():
+    url = (
+        "https://hh.ru/search/vacancy?L_save_area=true&text=python&excluded_text=&area=10"
+        "02&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_"
+        "period=0&items_on_page=50"
+    )
+    scraper = HhScraper()
+    filename = "hh_data.json"
+    scraper.run(url, filename)
+
+
+def run_hacker_news_scraper():
+    url = "https://news.ycombinator.com/"
+    filename = "hacker_news_data.json"
+    scraper = HackerNewsScraper()
+    scraper.run(url, filename)
+
+
+def run_rabota_scraper():
+    url = (
+        "https://rabota.by/search/vacancy?L_save_area=true&text=Python&excluded_text=&area"
+        "=1002&salary=&currency_code=BYR&experience=doesNotMatter&order_by=relevance&search"
+        "_period=0&items_on_page=50"
+    )
+    filename = "rabota_data.json"
+    scraper = RabotaScraper()
+    scraper.run(url, filename)
 
 
 def main():
     if len(sys.argv) > 1:
         if "nyt" in sys.argv:
-            print("Starting NYT scraper...")
-            url = "https://www.nytimes.com"
-            scraper = NYTimesScraperBS()
-            print(f"Fetching HTML content for {url}")
-            html_content = scraper.fetch_html(url)
-            print("Extracting data...")
-            articles_data = scraper.extract_data(html_content)
-            print(f"Extracted {len(articles_data)} articles")
-            print("Writing data to file...")
-            scraper.write_data(articles_data, "nyt_data.json")
-            print("NYT data scraping and writing complete.")
-
+            run_nyt_scraper()
         if "hack" in sys.argv:
-            print("Starting Hacker News scraper...")
-            scraper = HackerNewsScraper()
-            scraper.initialize_scraper()
-            scraper.navigate_to_page("https://news.ycombinator.com/")
-            data = scraper.extract_data()
-            scraper.write_data(data, "hacker_news_data.json")
-            scraper.driver.quit()
-            print("Hacker News data scraping and writing complete.")
-
+            run_hacker_news_scraper()
         if "rabota" in sys.argv:
-            print("Starting Rabota scraper...")
-            scraper = RabotaScraper()
-            scraper.initialize_scraper()
-            scraper.navigate_to_page(
-                "https://rabota.by/search/vacancy?L_save_area=true&text=Python&excluded_text=&area"
-                "=1002&salary=&currency_code=BYR&experience=doesNotMatter&order_by=relevance&search"
-                "_period=0&items_on_page=50"
-            )
-            data = scraper.extract_data()
-            scraper.write_data(data, "rabota_data.json")
-            scraper.driver.quit()
-            print("Rabota scraping and writing complete.")
-
+            run_rabota_scraper()
         if "hh" in sys.argv:
-            print("Starting HH scraper...")
-            url = (
-                "https://hh.ru/search/vacancy?L_save_area=true&text=python&excluded_text=&area=10"
-                "02&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_"
-                "period=0&items_on_page=50"
-            )
-            scraper = HhScraper()
-            print(f"Fetching HTML content for {url}")
-            html_content = scraper.fetch_html(url)
-            print("Extracting data...")
-            articles_data = scraper.extract_data(html_content)
-            print(f"Extracted {len(articles_data)} articles")
-            print("Writing data to file...")
-            scraper.write_data(articles_data, "hh_data.json")
-            print("HH data scraping and writing complete.")
+            run_hh_scraper()
+        else:
+            logger.warning("The specified argument is not supported.")
+    else:
+        logger.warning("No arguments were entered. Please provide an argument to start the scraper.")
 
 
 if __name__ == "__main__":
+    setup_logging()
     main()
